89bd68d8c9549c092d247ac355462c312bac4779
bennachan123 <bennachan2020@gmail.com>
2021-12-12 23:40:23 -0800

    calculator project

---
+++ calc.js
@@ -0,0 +1,12 @@
+function ClickDisplay(val)
+{
+document.getElementById("screen").value+=val
+}
+function cleardisplay(){
+    document.getElementById("screen").value=""
+}
+function EqualClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
